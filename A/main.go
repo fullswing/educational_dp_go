a0d4d005508c96b85559ba6cc3d1d22856ed71bf
fullswing <rs1018bio8.utokyo@gmail.com>
2021-09-16 02:00:00 +0900

    solve A

---
+++ A/main.go
@@ -0,0 +1,58 @@
+package main
+
+// https://atcoder.jp/contests/dp/tasks/dp_a
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const INF = math.MaxInt32
+
+func main() {
+	// 標準入力の準備
+	sc := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 0)
+	sc.Buffer(buf, 1010000)
+	sc.Split(bufio.ScanWords)
+	N := nextInt(sc)
+	hs := make([]int, N)
+	for i := 0; i < N; i++ {
+		hs[i] = nextInt(sc)
+	}
+	dp := make([]int, N)
+	dp[0] = 0
+	dp[1] = abs(hs[0], hs[1])
+	for i := 2; i < N; i++ {
+		cost_1 := dp[i-1] + abs(hs[i-1], hs[i])
+		cost_2 := dp[i-2] + abs(hs[i-2], hs[i])
+		dp[i] = min(cost_1, cost_2)
+	}
+	fmt.Println(dp[N-1])
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
